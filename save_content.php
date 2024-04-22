e92a7b749cb7a251561d51c95cf1f41f578f5c0d
Umair <m_umair69@yahoo.com>
2024-04-22 15:51:33 +0500

    Create save_content.php

---
+++ save_content.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the content from the POST data
+    $content = $_POST['content'];
+	$filename = $_POST['filename'];
+
+    // Save the content to a file (you can modify this part to save to a database, etc.)
+    $file = '_notes/' . $filename;
+    file_put_contents($file, $content);
+
+    // Send a response back to the client (optional)
+    echo 'Content saved successfully!';
+} else {
+    // Handle invalid requests
+    echo 'Invalid request';
+}
+?>
